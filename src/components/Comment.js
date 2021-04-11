60b0638b95c72d02fe633b57530a2dec0a450694
MiracleUFO <miracle.ufodiama.242949@unn.edu.ng>
2021-04-11 12:53:06 +0000

    Firebase integration

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { Context } from '../context';
-import { observer } from 'mobx-react-lite';
-import '../assets/css/feed.css';
-import '../assets/css/comments.css';
-import firebase from '../config/firebase';
+import React, { useState, useEffect, useContext } from "react";
+import { Context } from "../context";
+import { observer } from "mobx-react-lite";
+import "../assets/css/feed.css";
+import "../assets/css/comments.css";
+import firebase from "../config/firebase";
 
 const Comment = observer(function Comment() {
   const store = useContext(Context);
@@ -11,46 +11,466 @@ const Comment = observer(function Comment() {
   const [state, setState] = useState({
     id: store.index,
     comments: [],
-    commentVal: ''
+    commentVal: ""
   });
 
   useEffect(() => {
-    firebase.getComments(state.id).then(doc => {
-      console.log(doc.id, doc.data());
-    });
+    if (state.id !== undefined || state.id !== null) {
+      firebase.getComments(state.id).then((data) => {
+        data.forEach((comment) => {
+          let commentArr = [...state.comments, comment.data().comment];
+          setState({ ...state, comments: commentArr, commentVal: "" });
+        });
+      });
+    }
   }, [state.id]);
 
   useEffect(() => {
-    if(state.comments.length !== 0) {
-      let li = `<li><p>${state.comments[state.comments.length - 1]}</p><span>Anonymous</span></li>`;
-      document.getElementById('comments').innerHTML =  document.getElementById('comments').innerHTML + li;
+    if (state.comments.length !== 0) {
+      let li = `<li><p>${
+        state.comments[state.comments.length - 1]
+      }</p><span>Anonymous</span></li>`;
+      document.getElementById("comments").innerHTML =
+        document.getElementById("comments").innerHTML + li;
     }
-  }, [state.comments])
+  }, [state.comments]);
 
   let handleChange = (e) => {
-    setState({...state, commentVal: e.target.value})
-  }
+    setState({ ...state, commentVal: e.target.value });
+  };
 
   let handleSubmit = (e) => {
     e.preventDefault();
-    if(state.commentVal) {
-      let commentArr = [...state.comments, state.commentVal]
-      setState({...state, comments: commentArr, commentVal: ''}); 
+    if (state.commentVal) {
+      let commentArr = [...state.comments, state.commentVal];
+      setState({ ...state, comments: commentArr, commentVal: "" });
+      firebase.createComment({articleId: state.id, comment: state.commentVal});
     }
-  }
+  };
 
   return (
-    <section id='comment-container'>
+    <section id="comment-container">
       <h1>Comment</h1>
-      <ul id='comments'>
-        <li><p>Really nice piece</p><span>Anonymous</span></li>
+      <ul id="comments">
+        <li>
+        </li>
       </ul>
       <form onSubmit={handleSubmit}>
         <textarea value={state.commentVal} onChange={handleChange} />
-        <button className={state.commentVal ? 'button-active' : ''}>Submit</button>
+        <button className={state.commentVal ? "button-active" : ""}>
+          Submit
+        </button>
       </form>
     </section>
-  )
-})
+  );
+});
+
+export default Comment;
+import React, { useState, useEffect, useContext } from "react";
+import { Context } from "../context";
+import { observer } from "mobx-react-lite";
+import "../assets/css/feed.css";
+import "../assets/css/comments.css";
+import firebase from "../config/firebase";
+
+const Comment = observer(function Comment() {
+  const store = useContext(Context);
+
+  const [state, setState] = useState({
+    id: store.index,
+    comments: [],
+    commentVal: ""
+  });
+
+  useEffect(() => {
+    if (state.id !== undefined || state.id !== null) {
+      firebase.getComments(state.id).then((data) => {
+        data.forEach((comment) => {
+          let commentArr = [...state.comments, comment.data().comment];
+          setState({ ...state, comments: commentArr, commentVal: "" });
+        });
+      });
+    }
+  }, [state.id]);
+
+  useEffect(() => {
+    if (state.comments.length !== 0) {
+      let li = `<li><p>${
+        state.comments[state.comments.length - 1]
+      }</p><span>Anonymous</span></li>`;
+      document.getElementById("comments").innerHTML =
+        document.getElementById("comments").innerHTML + li;
+    }
+  }, [state.comments]);
+
+  let handleChange = (e) => {
+    setState({ ...state, commentVal: e.target.value });
+  };
+
+  let handleSubmit = (e) => {
+    e.preventDefault();
+    if (state.commentVal) {
+      let commentArr = [...state.comments, state.commentVal];
+      setState({ ...state, comments: commentArr, commentVal: "" });
+      firebase.createComment({articleId: state.id, comment: state.commentVal});
+    }
+  };
+
+  return (
+    <section id="comment-container">
+      <h1>Comment</h1>
+      <ul id="comments">
+        <li>
+        </li>
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <textarea value={state.commentVal} onChange={handleChange} />
+        <button className={state.commentVal ? "button-active" : ""}>
+          Submit
+        </button>
+      </form>
+    </section>
+  );
+});
+
+export default Comment;
+import React, { useState, useEffect, useContext } from "react";
+import { Context } from "../context";
+import { observer } from "mobx-react-lite";
+import "../assets/css/feed.css";
+import "../assets/css/comments.css";
+import firebase from "../config/firebase";
+
+const Comment = observer(function Comment() {
+  const store = useContext(Context);
+
+  const [state, setState] = useState({
+    id: store.index,
+    comments: [],
+    commentVal: ""
+  });
+
+  useEffect(() => {
+    if (state.id !== undefined || state.id !== null) {
+      firebase.getComments(state.id).then((data) => {
+        data.forEach((comment) => {
+          let commentArr = [...state.comments, comment.data().comment];
+          setState({ ...state, comments: commentArr, commentVal: "" });
+        });
+      });
+    }
+  }, [state.id]);
+
+  useEffect(() => {
+    if (state.comments.length !== 0) {
+      let li = `<li><p>${
+        state.comments[state.comments.length - 1]
+      }</p><span>Anonymous</span></li>`;
+      document.getElementById("comments").innerHTML =
+        document.getElementById("comments").innerHTML + li;
+    }
+  }, [state.comments]);
+
+  let handleChange = (e) => {
+    setState({ ...state, commentVal: e.target.value });
+  };
+
+  let handleSubmit = (e) => {
+    e.preventDefault();
+    if (state.commentVal) {
+      let commentArr = [...state.comments, state.commentVal];
+      setState({ ...state, comments: commentArr, commentVal: "" });
+      firebase.createComment({articleId: state.id, comment: state.commentVal});
+    }
+  };
+
+  return (
+    <section id="comment-container">
+      <h1>Comment</h1>
+      <ul id="comments">
+        <li>
+        </li>
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <textarea value={state.commentVal} onChange={handleChange} />
+        <button className={state.commentVal ? "button-active" : ""}>
+          Submit
+        </button>
+      </form>
+    </section>
+  );
+});
+
+export default Comment;
+import React, { useState, useEffect, useContext } from "react";
+import { Context } from "../context";
+import { observer } from "mobx-react-lite";
+import "../assets/css/feed.css";
+import "../assets/css/comments.css";
+import firebase from "../config/firebase";
+
+const Comment = observer(function Comment() {
+  const store = useContext(Context);
+
+  const [state, setState] = useState({
+    id: store.index,
+    comments: [],
+    commentVal: ""
+  });
+
+  useEffect(() => {
+    if (state.id !== undefined || state.id !== null) {
+      firebase.getComments(state.id).then((data) => {
+        data.forEach((comment) => {
+          let commentArr = [...state.comments, comment.data().comment];
+          setState({ ...state, comments: commentArr, commentVal: "" });
+        });
+      });
+    }
+  }, [state.id]);
+
+  useEffect(() => {
+    if (state.comments.length !== 0) {
+      let li = `<li><p>${
+        state.comments[state.comments.length - 1]
+      }</p><span>Anonymous</span></li>`;
+      document.getElementById("comments").innerHTML =
+        document.getElementById("comments").innerHTML + li;
+    }
+  }, [state.comments]);
+
+  let handleChange = (e) => {
+    setState({ ...state, commentVal: e.target.value });
+  };
+
+  let handleSubmit = (e) => {
+    e.preventDefault();
+    if (state.commentVal) {
+      let commentArr = [...state.comments, state.commentVal];
+      setState({ ...state, comments: commentArr, commentVal: "" });
+      firebase.createComment({articleId: state.id, comment: state.commentVal});
+    }
+  };
+
+  return (
+    <section id="comment-container">
+      <h1>Comment</h1>
+      <ul id="comments">
+        <li>
+        </li>
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <textarea value={state.commentVal} onChange={handleChange} />
+        <button className={state.commentVal ? "button-active" : ""}>
+          Submit
+        </button>
+      </form>
+    </section>
+  );
+});
+
+export default Comment;
+import React, { useState, useEffect, useContext } from "react";
+import { Context } from "../context";
+import { observer } from "mobx-react-lite";
+import "../assets/css/feed.css";
+import "../assets/css/comments.css";
+import firebase from "../config/firebase";
+
+const Comment = observer(function Comment() {
+  const store = useContext(Context);
+
+  const [state, setState] = useState({
+    id: store.index,
+    comments: [],
+    commentVal: ""
+  });
+
+  useEffect(() => {
+    if (state.id !== undefined || state.id !== null) {
+      firebase.getComments(state.id).then((data) => {
+        data.forEach((comment) => {
+          let commentArr = [...state.comments, comment.data().comment];
+          setState({ ...state, comments: commentArr, commentVal: "" });
+        });
+      });
+    }
+  }, [state.id]);
+
+  useEffect(() => {
+    if (state.comments.length !== 0) {
+      let li = `<li><p>${
+        state.comments[state.comments.length - 1]
+      }</p><span>Anonymous</span></li>`;
+      document.getElementById("comments").innerHTML =
+        document.getElementById("comments").innerHTML + li;
+    }
+  }, [state.comments]);
+
+  let handleChange = (e) => {
+    setState({ ...state, commentVal: e.target.value });
+  };
+
+  let handleSubmit = (e) => {
+    e.preventDefault();
+    if (state.commentVal) {
+      let commentArr = [...state.comments, state.commentVal];
+      setState({ ...state, comments: commentArr, commentVal: "" });
+      firebase.createComment({articleId: state.id, comment: state.commentVal});
+    }
+  };
+
+  return (
+    <section id="comment-container">
+      <h1>Comment</h1>
+      <ul id="comments">
+        <li>
+        </li>
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <textarea value={state.commentVal} onChange={handleChange} />
+        <button className={state.commentVal ? "button-active" : ""}>
+          Submit
+        </button>
+      </form>
+    </section>
+  );
+});
+
+export default Comment;
+import React, { useState, useEffect, useContext } from "react";
+import { Context } from "../context";
+import { observer } from "mobx-react-lite";
+import "../assets/css/feed.css";
+import "../assets/css/comments.css";
+import firebase from "../config/firebase";
+
+const Comment = observer(function Comment() {
+  const store = useContext(Context);
+
+  const [state, setState] = useState({
+    id: store.index,
+    comments: [],
+    commentVal: ""
+  });
+
+  useEffect(() => {
+    if (state.id !== undefined || state.id !== null) {
+      firebase.getComments(state.id).then((data) => {
+        data.forEach((comment) => {
+          let commentArr = [...state.comments, comment.data().comment];
+          setState({ ...state, comments: commentArr, commentVal: "" });
+        });
+      });
+    }
+  }, [state.id]);
+
+  useEffect(() => {
+    if (state.comments.length !== 0) {
+      let li = `<li><p>${
+        state.comments[state.comments.length - 1]
+      }</p><span>Anonymous</span></li>`;
+      document.getElementById("comments").innerHTML =
+        document.getElementById("comments").innerHTML + li;
+    }
+  }, [state.comments]);
+
+  let handleChange = (e) => {
+    setState({ ...state, commentVal: e.target.value });
+  };
+
+  let handleSubmit = (e) => {
+    e.preventDefault();
+    if (state.commentVal) {
+      let commentArr = [...state.comments, state.commentVal];
+      setState({ ...state, comments: commentArr, commentVal: "" });
+      firebase.createComment({articleId: state.id, comment: state.commentVal});
+    }
+  };
+
+  return (
+    <section id="comment-container">
+      <h1>Comment</h1>
+      <ul id="comments">
+        <li>
+        </li>
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <textarea value={state.commentVal} onChange={handleChange} />
+        <button className={state.commentVal ? "button-active" : ""}>
+          Submit
+        </button>
+      </form>
+    </section>
+  );
+});
+
+export default Comment;
+import React, { useState, useEffect, useContext } from "react";
+import { Context } from "../context";
+import { observer } from "mobx-react-lite";
+import "../assets/css/feed.css";
+import "../assets/css/comments.css";
+import firebase from "../config/firebase";
+
+const Comment = observer(function Comment() {
+  const store = useContext(Context);
+
+  const [state, setState] = useState({
+    id: store.index,
+    comments: [],
+    commentVal: ""
+  });
+
+  useEffect(() => {
+    if (state.id !== undefined || state.id !== null) {
+      firebase.getComments(state.id).then((data) => {
+        data.forEach((comment) => {
+          let commentArr = [...state.comments, comment.data().comment];
+          setState({ ...state, comments: commentArr, commentVal: "" });
+        });
+      });
+    }
+  }, [state.id]);
+
+  useEffect(() => {
+    if (state.comments.length !== 0) {
+      let li = `<li><p>${
+        state.comments[state.comments.length - 1]
+      }</p><span>Anonymous</span></li>`;
+      document.getElementById("comments").innerHTML =
+        document.getElementById("comments").innerHTML + li;
+    }
+  }, [state.comments]);
+
+  let handleChange = (e) => {
+    setState({ ...state, commentVal: e.target.value });
+  };
+
+  let handleSubmit = (e) => {
+    e.preventDefault();
+    if (state.commentVal) {
+      let commentArr = [...state.comments, state.commentVal];
+      setState({ ...state, comments: commentArr, commentVal: "" });
+      firebase.createComment({articleId: state.id, comment: state.commentVal});
+    }
+  };
+
+  return (
+    <section id="comment-container">
+      <h1>Comment</h1>
+      <ul id="comments">
+        <li>
+        </li>
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <textarea value={state.commentVal} onChange={handleChange} />
+        <button className={state.commentVal ? "button-active" : ""}>
+          Submit
+        </button>
+      </form>
+    </section>
+  );
+});
 
 export default Comment;

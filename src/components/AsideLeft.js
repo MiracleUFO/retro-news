420e9f0aab99d3e77c9d52156411f0b0186dd555
MircleUFO <miracleufodiama@gmail.com>
2021-05-04 22:06:00 +0100

    Style bug fix

--- src/components/AsideLeft.js
+++ src/components/AsideLeft.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useEffect, useContext } from 'react';
 import '../assets/css/feed.css';
 import   { Context } from '../context';
 import { NavLink } from 'react-router-dom';
@@ -6,11 +6,17 @@ import { NavLink } from 'react-router-dom';
 const AsideLeft = (props) => {
   const store = useContext(Context);
 
+  useEffect(() => {
+    document.getElementById('first-tt').innerHTML = `${store.articles[0].fields.trailText}..`;
+    document.getElementById('second-tt').innerHTML = `${store.articles[1].fields.trailText}..`
+  }, []) //eslint-disable-line react-hooks/exhaustive-deps
+
   let handleClick = (e) => {
     e.preventDefault();
     let index = Number(e.target.getAttribute('data-attribute-index'));
     store.setSelected(index);
   }
+
 
   return (
     <aside id='aside-left'>
@@ -20,7 +26,7 @@ const AsideLeft = (props) => {
             <img src={store.articles[0].fields.thumbnail} alt='Latest story' data-attribute-index={0}/>
             <div className='flex-column' data-attribute-index={0}>
               <h1 data-attribute-index={0}>{store.articles[0].fields.headline}</h1>
-              <p data-attribute-index={0}>{store.articles[0].fields.trailText}..</p>
+              <p data-attribute-index={0} id='first-tt'></p>
             </div>
           </NavLink>
           }
@@ -33,7 +39,7 @@ const AsideLeft = (props) => {
             <img src={store.articles[1].fields.thumbnail} className='thumb-img' alt='Latest story' data-attribute-index={1} onClick={props.handleClick} />
             <h1 className='thumb-text' data-attribute-index={1}>{store.articles[1].fields.headline}</h1>
           </div>
-          <p data-attribute-index={1}>{store.articles[1].fields.trailText}...</p>
+          <p data-attribute-index={1} id='second-tt'></p>
         </NavLink>
         }
         </div>
--- src/components/AsideRight.js
+++ src/components/AsideRight.js
@@ -1,10 +1,15 @@
-import React, { useContext } from 'react';
+import React, { useEffect, useContext } from 'react';
 import '../assets/css/feed.css';
 import   { Context } from '../context';
 import { NavLink } from 'react-router-dom';
 
 const AsideRight = (props) => {
   const store = useContext(Context);
+
+  useEffect(() => {
+    document.getElementById('third-tt').innerHTML = `${store.articles[2].fields.trailText}..`;
+    document.getElementById('fourth-tt').innerHTML = `${store.articles[3].fields.trailText}..`
+  }, []) //eslint-disable-line react-hooks/exhaustive-deps
 
   let handleClick = (e) => {
     e.preventDefault();
@@ -21,7 +26,7 @@ const AsideRight = (props) => {
           <img src={store.articles[2].fields.thumbnail} className='thumb-img' alt='Latest story' data-attribute-index={2} />
           <h1 className='thumb-text' data-attribute-index={2}>{store.articles[2].fields.headline}</h1>
         </div>
-        <p data-attribute-index={2}>{store.articles[2].fields.trailText}...</p>
+        <p data-attribute-index={2} id='third-tt'></p>
       </NavLink>
       }
       </div>
@@ -32,7 +37,7 @@ const AsideRight = (props) => {
           <img src={store.articles[3].fields.thumbnail} alt='Latest story' data-attribute-index={3}/>
           <div className='flex-column' data-attribute-index={3}>
             <h1 data-attribute-index={3}>{store.articles[3].fields.headline}</h1>
-            <p data-attribute-index={3}>{store.articles[3].fields.trailText}..</p>
+            <p data-attribute-index={3} id='fourth-tt'></p>
           </div>
         </NavLink>
         }
